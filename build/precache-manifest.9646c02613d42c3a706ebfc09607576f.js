df210bfcd21587f9c9ccfd5c6f1095da0d8cd658
HopeStaff <gazm.net@gmail.com>
2020-08-25 22:56:51 +0200

    build update 25 aug 20 22.56

---
+++ build/precache-manifest.9646c02613d42c3a706ebfc09607576f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e28f7c02822ddb483b000214b7e940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28d65960e0ff1f4f68a",
+    "url": "/static/css/main.134e9fdd.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28d65960e0ff1f4f68a",
+    "url": "/static/js/main.a29f7fcf.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
